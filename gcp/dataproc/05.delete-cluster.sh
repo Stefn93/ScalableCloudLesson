205b1cde7a5691a83cccda283be938805675c947
GloriaSam <gloriasammaritani@gmail.com>
2020-11-25 22:05:34 +0100

    fixed import

--- gcp/dataproc/05.delete-cluster.sh
+++ gcp/dataproc/05.delete-cluster.sh
@@ -2,6 +2,6 @@
 
 source ./00.variables.sh
 
-./90.log-time.sh "DELETING CLUSTER '${DATAPROC_CLUSTER_NAME}' ..."
+sh 90.log-time.sh "DELETING CLUSTER '${DATAPROC_CLUSTER_NAME}' ..."
 gcloud dataproc clusters delete -q ${DATAPROC_CLUSTER_NAME} --region ${DATAPROC_CLUSTER_REGION}
-./90.log-time.sh "CLUSTER '${DATAPROC_CLUSTER_NAME}' DELETED!"
+sh 90.log-time.sh "CLUSTER '${DATAPROC_CLUSTER_NAME}' DELETED!"
