291f599413640d676bbf5db847a5cce70681c1f0
stefn <stefano.romanazzi@injenia.it>
2022-12-01 15:15:12 +0100

    script reformatted

--- gcp/streaming_beam/scripts/pipeline_build.sh
+++ gcp/streaming_beam/scripts/pipeline_build.sh
@@ -1,6 +1,6 @@
-gcloud builds submit --tag "$TEMPLATE_IMAGE" .
+gcloud builds submit --tag "$TEMPLATE_IMAGE" . 
 
 gcloud dataflow flex-template build $TEMPLATE_PATH \
   --image "$TEMPLATE_IMAGE" \
   --sdk-language "PYTHON" \
-  --metadata-file "metadata.json"
+  --metadata-file "metadata.json" 
